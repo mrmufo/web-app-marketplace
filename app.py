06375170cd3aa42b34c59d63609a08dadbc218c6
Wojciech Rykala <voichefr@gmail.com>
2019-02-20 13:22:04 +0000

    Added new_ad form

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import (
     Flask,
     render_template,
+    redirect,
+    url_for,
 )
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/new_ad')
+def new_ad_form():
+    return render_template('new_ad.html')
+
+
+@app.route('/new_ad', methods=['POST'])
+def post_new_ad():
+    return redirect(url_for('/'))
 
 
 if __name__ == "__main__":
