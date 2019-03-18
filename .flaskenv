9a9528a86ad63f51b292eaff7ed73eb003e33b6c
Wojciech Rykala <voichefr@gmail.com>
2019-03-18 14:21:23 +0000

    Changed project structure

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+export FLASK_APP=bsrh.py
+export FLASK_ENV=development
--- bsrh.py
+++ bsrh.py
@@ -1,26 +1,22 @@
-from flask import (
-    Flask,
-    render_template,
-    redirect,
-    url_for,
-)
-from flask_sqlalchemy import SQLAlchemy
+from app import app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mufo/FlaskProjects/BSRH/bsrh.db'
-db = SQLAlchemy(app)
-db.create_all()
+# from flask_sqlalchemy import SQLAlchemy
 
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/mufo/FlaskProjects/BSRH/bsrh.db'
+# db = SQLAlchemy(app)
+# db.create_all()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-
-admin = User(username='admin', email='admin@example.com')
-guest = User(username='guest', email='guest@example.com')
+#
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     category = db.Column(db.String(80), unique=True, nullable=False)
+#     email = db.Column(db.String(120), unique=True, nullable=False)
+#
+#
+# admin = User(username='admin', email='admin@example.com')
+# guest = User(username='guest', email='guest@example.com')
 
 
 # if __name__ == "__main__":
