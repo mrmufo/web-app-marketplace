287f535ca9d0a94de3c3f9be34e3462d4a425c64
Wojciech Rykala <voichefr@gmail.com>
2019-03-22 13:09:20 +0000

    E/Changed User table relationship, added users and ads to bsrh.db

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    advertisements = db.relationship('Post', backref='author', lazy='dynamic')
+    advertisements = db.relationship('Ads', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
