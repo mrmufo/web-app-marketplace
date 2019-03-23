5ab845871869006b39fb5daddbf7d19a7e974060
Wojciech Rykala <voichefr@gmail.com>
2019-03-23 13:21:00 +0000

    Added user login & logout

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,10 @@ from flask import (
     url_for,
     flash
 )
+from flask_login import current_user, login_user, logout_user
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -25,22 +27,20 @@ def post_new_ad():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        print("Jestem")
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('home'))
-    # user = {'username': 'Miguel'}
-    # posts = [
-    #     {
-    #         'author': {'username': 'John'},
-    #         'body': 'Beautiful day in Portland!'
-    #     },
-    #     {
-    #         'author': {'username': 'Susan'},
-    #         'body': 'The Avengers movie was so cool!'
-    #     }
-    # ]
-    print("nieJestem")
     return render_template('login.html', title='Sign in', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
