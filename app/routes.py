83ad148405d045954cd6b348a471858fd108c366
Wojciech Rykala <voichefr@gmail.com>
2019-04-12 13:15:19 +0100

    Added registration template & view function

--- app/routes.py
+++ app/routes.py
@@ -12,8 +12,8 @@ from flask_login import (
     login_required
 )
 from werkzeug.urls import url_parse
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -67,3 +67,18 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Great, your account has been created! You can BSRH now!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
