7508b79eff8ac75465acfb86f1cce0f466498a52
Wojciech Rykala <voichefr@gmail.com>
2019-04-15 19:24:06 +0100

    Added logging to a file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-import logging
-from logging.handlers import SMTPHandler
+import logging, os
+from logging.handlers import RotatingFileHandler, SMTPHandler
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -33,3 +33,15 @@ if not app.debug:
                 credentials=auth, secure=secure)
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/bsrh.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('BSRH startup')
