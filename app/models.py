b7d93ebcb253deeb1ea6ff78e561593283262651
Wojciech Rykala <voichefr@gmail.com>
2019-04-15 17:24:47 +0100

    Upgraded profile page, edit profile, extended database, changed nav look

--- app/models.py
+++ app/models.py
@@ -4,12 +4,15 @@ from app import db, login
 from flask_login import UserMixin
 from hashlib import md5
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     ads = db.relationship('Ad', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -11,10 +11,11 @@ from flask_login import (
     logout_user,
     login_required
 )
-from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import EditProfileForm, LoginForm, RegistrationForm
 from app.models import User
+from datetime import datetime
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
@@ -93,3 +94,26 @@ def user(username):
         {'author': user, 'content': 'Test post #2'}
     ]
     return render_template('user.html', user=user, ads=ads)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changed have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
