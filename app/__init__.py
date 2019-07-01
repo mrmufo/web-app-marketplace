8c1fcf777bcb158f7386776568e00e7f5dfe1d49
Wojciech Rykala <voichefr@gmail.com>
2019-07-01 23:58:12 +0100

    Started Timezone conversions with Flask-Moment and moment.js

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 from config import Config
 
 
@@ -19,6 +20,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
