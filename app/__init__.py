20a04814c294e7acbcac4cf8a3de17404b733f8a
Wojciech Rykala <voichefr@gmail.com>
2019-07-26 19:51:10 +0100

    Added date and time translation, customized pybabel commands.

--- app/__init__.py
+++ app/__init__.py
@@ -59,6 +59,6 @@ if not app.debug:
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-    # return 'pl'
+    #  return 'pl'
 
 from app import errors, models, routes
--- bsrh.py
+++ bsrh.py
@@ -1,7 +1,8 @@
-from app import app, db
+from app import app, db, cli
 from app.models import User, Ad
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Ad': Ad}
+
