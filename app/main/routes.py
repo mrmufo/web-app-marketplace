ef8b00dd0a147b25e4cf005bc9879ce9cdea6408
Wojciech Rykala <voichefr@gmail.com>
2020-01-30 17:53:13 +0000

    Search functionality is working using Elasticsearch

--- app/main/routes.py
+++ app/main/routes.py
@@ -142,12 +142,12 @@ def search():
     if not g.search_form.validate():
         return redirect(url_for('main.explore'))
     page = request.args.get('page', 1, type=int)
-    posts, total = Ad.search(g.search_form.q.data, page, current_app.config['POSTS_PER_PAGE'])
+    ads, total = Ad.search(g.search_form.q.data, page, current_app.config['ADS_PER_PAGE'])
     next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
-        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+        if total > page * current_app.config['ADS_PER_PAGE'] else None
     prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
         if page > 1 else None
-    return render_template('search.html', title=_('Search'), posts=posts,
+    return render_template('search.html', title=_('Search'), ads=ads,
                            next_url=next_url, prev_url=prev_url)
 
 
