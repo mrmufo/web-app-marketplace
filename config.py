10ca8142061f0f9a3033d6668781df78ae5f429b
Wojciech Rykala <voichefr@gmail.com>
2020-07-13 22:25:27 +0100

    STARTTLS problem fixed

--- config.py
+++ config.py
@@ -11,15 +11,15 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'bsrh.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_USE_TLS = False  # os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = True  # os.environ.get('MAIL_USE_SSL')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ADMINS = ['buysellrenthire@gmail.com']
-    ADS_PER_PAGE = 3
+    ADS_PER_PAGE = 5
     LANGUAGES = ['en', 'es']
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
