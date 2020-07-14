44e993ec824693db04abfab072577ca9e4899bab
Wojciech Rykala <voichefr@gmail.com>
2020-07-14 20:22:34 +0100

    Updating ES with reindex()

--- app/models.py
+++ app/models.py
@@ -49,7 +49,7 @@ class SearchableMixin(object):
 
 db.event.listen(db.session, 'before_commit', SearchableMixin.before_commit)
 db.event.listen(db.session, 'after_commit', SearchableMixin.after_commit)
-db.event.listen(db.session, 'reindex', SearchableMixin.reindex())
+db.event.listen(db.session, 'reindex', SearchableMixin.reindex)
 
 followers = db.Table('followers',
                      db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
